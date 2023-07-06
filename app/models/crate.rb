277a3dc49c00fa65dda98fe87e3e19d932df2d18
007hillman <ayuktataw009@gmail.com>
2023-07-06 07:29:47 +0100

    added devise  gem to the app and started configuring it

--- app/models/crate.rb
+++ app/models/crate.rb
@@ -1,3 +1,22 @@
 class Crate < ApplicationRecord
   belongs_to :client
+
+  def self.brassseries_crates_needed(command)
+    crates_needed = 0
+    command.items.each do |item|
+      if(Supplier.find(item.drink.supplyer).name == "Brasseries du Cameroun SA" and item.drink.packaging == "crate")
+      crates_needed += item.quantity
+      end 
+    end
+    return crates_needed
+  end
+  def self.guinness_crates_needed(command)
+    crates_needed = 0
+    command.items.each do |item|
+      if(Supplier.find(item.drink.supplyer).name == "Guinness Cameroun SA" and item.drink.packaging == "crate")
+      crates_needed += item.quantity
+      end 
+    end
+    return crates_needed 
+  end
 end
