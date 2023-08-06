1f0fa6126785227dd9a1eb420b86169e609875cc
007hillman <ayuktataw009@gmail.com>
2023-08-06 13:31:00 +0100

    bug fixes

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -4,6 +4,7 @@ module ClientsHelper
     def total_owed(client_name)
         sum = 0
         @commands = Command.global_search(client_name)
+        puts client_name + "......................................"
         c= @commands.last
         sum = Client.total_owed(client_name, c.created_at)
         return sum
