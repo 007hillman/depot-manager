32b5d132fda935d406f5ea717416c66794879e7d
007hillman <tatawhillman@gmail.com>
2024-02-16 11:09:44 +0100

    change of interface

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    
+  }
+  show_menu(){
+    var item = document.querySelector('#menu_div')
+    item.classList.toggle("hidden")
+  }
+  up(e){
+    e.preventDefault()
+    var quantity_field = document.querySelector('#quantity')
+    quantity_field.value = parseInt(quantity_field.value, 10) + 1
+  }
+  down(e){
+    e.preventDefault()
+    var quantity_field = document.querySelector('#quantity')
+    quantity_field.value = parseInt(quantity_field.value, 10) - 1
   }
 }
